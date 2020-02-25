15ba03572bcf1a62a9dcf76bdf8567d2702d486f
DanielRolewski <daniel_rolewski@outlook.com>
2020-02-25 20:25:12 +0000

    prepare base

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express')
-const nodemailer = require("nodemailer");
-const exphbs  = require('express-handlebars');
+const nodemailer = require('nodemailer');
+const path = require('path');
 const port = 3000;
 
+const router = express.Router();
 const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile( './../public/views/index.html'));
+});
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+exports.app = functions.https.onRequest(app);
