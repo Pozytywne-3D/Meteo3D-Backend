f1e9a30deaeb985952f9f9cff8f117c5d9080a97
DanielRolewski <daniel_rolewski@outlook.com>
2020-02-27 09:59:47 +0000

    init

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,34 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express')
-const nodemailer = require('nodemailer');
-const path = require('path');
-const port = 3000;
+const firebase = require('firebase-admin');
+const express = require('express');
+const engines = require('consolidate');
+//const nodemailer = require('nodemailer');
+//const port = 3000;
 
-const router = express.Router();
+const firebaseApp = firebase.initializeApp(
+	functions.config().firebase
+);
+
 const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
 
+app.use('/static',express.static('public'));
+
+app.get('/', (request, response) => {
+	response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+		response.render('index', { text: 'handlebarsy też działają'});
+});
+
+
+/*
+app.static(path.join(__dirname + "public"))
 app.get('/', (req, res) => {
-  res.sendFile( './../public/views/index.html'));
+  res.sendFile( '/public/views/index.html'));
 });
 
 app.use('/', router);
 app.listen(process.env.port || 3000);
+*/
 exports.app = functions.https.onRequest(app);
